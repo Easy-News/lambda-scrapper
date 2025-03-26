dde2d3455c2d7f01106151dc82855e2f48e917fa
twkwon0417 <kwontony123@konkuk.ac.kr>
2025-03-26 12:41:01 +0900

    feat: add actual datasource

--- main.go
+++ main.go
@@ -5,17 +5,61 @@ import (
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
-
 	"log"
+	"os"
 )
 
 func main() {
-	db, err := sql.Open("mysql", "root:9036@tcp(127.0.0.1:3306)/example-db") // 1
+	db, err := sql.Open(os.Getenv("DB"), os.Getenv("DATA_SOURCE")) // 1
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("DB 연동: %+v\n", db.Stats())    // 2
-	db.Close()                                // 3
-	fmt.Printf("DB 연동 종료: %+v\n", db.Stats()) // 4
+	fmt.Printf("DB 연동: %+v\n", db.Stats())
+
+	result, err := db.Query("SHOW GLOBAL VARIABLES LIKE 'max_%resultect%'") // 1
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("resultection 생성: %+v\n", db.Stats()) // 2
+
+	for result.Next() { // 3
+		name := ""
+		value := ""
+		if err := result.Scan(&name, &value); err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(name, value)
+	}
+
+	result, err = db.Query("SELECT * FROM instructor")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for result.Next() {
+		var id int
+		var name string
+		var dept_name string
+		var salary int
+
+		if err := result.Scan(&id, &name, &dept_name, &salary); err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(id, name, dept_name, salary)
+	}
+
+	_, err = db.Exec(
+		"INSERT INTO instructor (id, name, dept_name, salary) VALUES (7, 'gopher', 'CSE', 10000000)",
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	result.Close() // 4
+	fmt.Printf("resultection 연결 종료: %+v\n", db.Stats())
+
+	db.Close()
+	fmt.Printf("DB 연동 종료: %+v\n", db.Stats())
 }
