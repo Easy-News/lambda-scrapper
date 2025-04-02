5dbd947a2bb0555dd5aff423fdd78178c32f14dd
twkwon0417 <kwontony123@konkuk.ac.kr>
2025-04-02 20:10:02 +0900

    refactor: add user agent to shirk

--- main.go
+++ main.go
@@ -36,7 +36,15 @@ func collectLinks(category Category, ch chan<- urlWrapper, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	var urls []string
-	c := colly.NewCollector()
+	c := colly.NewCollector(
+		colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+			"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"),
+	)
+
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+		r.Headers.Set("Accept-Language", "en-US,en;q=0.9")
+	})
 
 	c.OnHTML("ul[id*='_SECTION_HEADLINE_LIST_'] .sa_text a[class*='sa_text_title']", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
@@ -98,13 +106,11 @@ type urlWrapper struct {
 	category string
 }
 
-// Handler is the AWS Lambda entry point.
 func Handler(ctx context.Context) (string, error) {
-	// Initialize DB connection.
 	initDB()
 	defer db.Close()
 
-	stmt, err := db.Prepare("INSERT INTO news (title, content, category) VALUES (?, ?, ?)")
+	stmt, err := db.Prepare("INSERT INTO news (title, content, category) VALUES ($1, $2, $3)")
 	if err != nil {
 		log.Fatal(err)
 		return "", err // Lambda의 경우 오류가 났다고 알려줘야 해
